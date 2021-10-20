7925fb5fe66a9d9834c4f902650fd00572e8164d
Arnab-Mukhopadhyay-hash <84343247+Arnab-Mukhopadhyay-hash@users.noreply.github.com>
2021-10-20 19:34:21 +0530

    Add files via upload

---
+++ Programs in C/Add_Two_Numbers(FLOW001).c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int a, b, t, sum;
+    scanf("%d", &t);
+    for(int i = 0; i < t; i++)
+    {
+        scanf("%d %d", &a, &b);
+        sum = a + b;
+        printf("%d\n", sum);
+    }
+    return 0;
+}
