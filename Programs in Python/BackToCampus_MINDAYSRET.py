12fe12bc17fb9ad25cb12eecbc0d9c4afc6b49c9
Arnab-Mukhopadhyay-hash <84343247+Arnab-Mukhopadhyay-hash@users.noreply.github.com>
2021-11-23 23:54:25 +0530

    Update BackToCampus_MINDAYSRET.py

--- Programs in Python/BackToCampus_MINDAYSRET.py
+++ Programs in Python/BackToCampus_MINDAYSRET.py
@@ -1,19 +1,5 @@
 for _ in range(int(input())):
     n, k = map(int, input().split())
-    # days = 0
-    # if n == k or n < k:
-    #     days = 1
-    # else:
-    #     if n - k <= k:
-    #         days = 2
-    #     else:
-    #         days = 2
-    #         days += (n // k)
-    # print(days)
-
-
-    # days = n//k
-    # print(days)
 
     days = 0
     while n > 0:
